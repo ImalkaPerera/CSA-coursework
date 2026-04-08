161fc6fd1e6d84a1939f09eb70753c83198893d1
ImalkaPerera <ImalkaPerera@users.noreply.github.com>
2026-04-08 10:20:13 +0530

    feat: setup Grizzly server bootstrap and JAX-RS application configuration

---
+++ src/main/java/com/smartcampus/Main.java
@@ -0,0 +1,30 @@
+package com.smartcampus;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    public static HttpServer startServer() {
+        // Create a resource config that scans for JAX-RS resources and providers
+        // in com.smartcampus package
+        final ResourceConfig rc = ResourceConfig.forApplicationClass(SmartCampusApplication.class);
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Smart Campus API started at %sapi/v1", BASE_URI));
+        System.out.println("Hit enter to stop it...");
+        System.in.read();
+        server.shutdownNow();
+    }
+}
---
+++ src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -0,0 +1,16 @@
+package com.smartcampus;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        // Resources will be registered here in later days
+        return classes;
+    }
+}
